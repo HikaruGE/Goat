41f7dd08cd7c5ecdc9dfb9e5c1cc0b6b2ac3ec85
yigewen <wenge907@gmail.com>
2019-04-10 19:46:16 +1000

    Revise Ast

--- GoatAST.hs
+++ GoatAST.hs
@@ -8,19 +8,23 @@ type Ident = String
 
 data Program
   = Program [Proc]
+    deriving (Show, Eq)
 
 data Proc
   = Proc Ident [Param] [Decl] [Stmt]
+    deriving (Show, Eq)
 
 data Param
   = Param Indic BaseType Ident
+    deriving (Show, Eq)
 
 data Decl
   = Decl BaseType Ident
+    deriving (Show, Eq)
 
 data Stmt 
-  = Assign Lvalue Expr
-  | Read Lvalue
+  = Assign Ident Expr
+  | Read Var
   | Write Expr
   | Call Ident [Expr]
   | If Expr [Stmt]
@@ -29,41 +33,54 @@ data Stmt
     deriving (Show, Eq)
 
 data Expr
-  = BoolConst Bool
-  | IntConst Int
-  | FloatConst Float
-  | StrConst String
-  | Id Ident
-  | Unary UnaOp Exp
-  | Binary BinOp Exp Exp
+  = Const Const
+  --   BoolConst Bool
+  -- | IntConst Int
+  -- | FloatConst Float
+  -- | StrConst String
+
+  -- | Id Ident
+  -- | Array Ident Expr
+  -- | Matrix Ident Expr Expr
+  | Var Var
+  | Unary UnaOp Expr
+  | Binary BinOp Expr Expr
+    deriving (Show, Eq)
 
 data BinOp
-  = Add
-  | Sub
-  | Mul
-  | Div
-  | And
-  | Or
-  | Eq
-  | NotEq
-  | Lt
-  | LtEq
-  | Gt
-  | GtEq
-  deriving (Show, Eq)
+  = Add | Sub | Mul | Div
+  | And | Or 
+  | Eq | NotEq
+  | Lt | LtEq
+  | Gt | GtEq
+    deriving (Show, Eq)
 
 data UnaOp
   = Neg
   | Minus
-  deriving (Show, Eq)
+    deriving (Show, Eq)
 
 data Indic
   = Val | Ref
+    deriving (Show, Eq)
 
 data BaseType 
   = BoolType | IntType | FloatType
     deriving (Show, Eq)
 
-data Lvalue 
-  = LId Ident
+-- data Lvalue 
+--   = LId Ident
+--     deriving (Show, Eq)
+
+data Var
+  = Id Ident
+  | Array Ident Expr
+  | Matrix Ident Expr Expr
+    deriving (Show, Eq)
+
+data Const
+  = BoolConst Bool
+  | IntConst Int
+  | FloatConst Float
+  | StrConst String
     deriving (Show, Eq)
