7a14d3ff28615eec08bc3e8f73d0f8cdfb81a092
reenausss <38513759+reenausss@users.noreply.github.com>
2019-04-15 17:44:18 +1000

    Update PrettyPrinter.hs

--- PrettyPrinter.hs
+++ PrettyPrinter.hs
@@ -2,6 +2,9 @@ module PrettyPrinter where
 
 import GoatAST
 
+-- The main function which takes an input of type Program and returns a well-formed string.
+-- Each pair of consecutive procedures are separated by a single blank line.
+-- There is only one newline character after the last procedure, instead of a blank line.
 pPrint :: Program -> String
 pPrint (Program [])
   = ""
@@ -10,18 +13,22 @@ pPrint (Program [x])
 pPrint (Program (x:y:ys))
   = (procPrint x) ++ "\n" ++ (pPrint (Program (y:ys)))
 
+-- Function which transforms a procedure into a string.
 procPrint :: Proc -> String
 procPrint (Proc a b c d)
   = (headerPrint a b) ++ (foldl indent "" (map declPrint c)) ++ "begin\n" ++ (foldl (++) "" (map (stmtPrint 4) d)) ++ "end\n"
 
+-- Default indentation which is four white spaces.
 indent :: String -> String -> String
 indent a b
   = a ++ "    " ++ b
 
+-- Function which transforms the header part into a string.
 headerPrint :: Ident -> [Param] -> String
 headerPrint a b
   = "proc" ++ " " ++ a ++ " " ++ "(" ++ (paramListPrint b) ++ ")\n"
 
+-- Function which transforms the parameter list in header into a string.
 paramListPrint :: [Param] -> String
 paramListPrint []
   = ""
@@ -30,16 +37,19 @@ paramListPrint [x]
 paramListPrint (x:y:ys)
   = (paramPrint x) ++ ", " ++ (paramListPrint (y:ys))
 
+-- Function which transforms a single parameter into a string.
 paramPrint :: Param -> String
 paramPrint (Param a b c)
   = (indicPrint a) ++ " " ++ (baseTypePrint b) ++ " " ++ c
 
+-- Function which transforms a parameter passing indicator into a string.
 indicPrint :: Indic -> String
 indicPrint Val
   = "val"
 indicPrint Ref
   = "ref"
 
+-- Function which transforms a base type into a string.
 baseTypePrint :: BaseType -> String
 baseTypePrint BoolType
   = "bool"
@@ -48,6 +58,7 @@ baseTypePrint IntType
 baseTypePrint FloatType
   = "float"
 
+-- Function which transforms a declaration into a string.
 declPrint :: Decl -> String
 declPrint (DeclVar a b)
   = (baseTypePrint a) ++ " " ++ b ++ ";\n"
@@ -56,6 +67,7 @@ declPrint (DeclArray a b c)
 declPrint (DeclMatrix a b c d)
   = (baseTypePrint a) ++ " " ++ b ++ "[" ++ (show c) ++ ", " ++ (show d) ++ "];\n"
 
+-- Function which transforms a statement into a string.
 stmtPrint :: Int -> Stmt -> String
 stmtPrint n (Assign a b)
   = (replicate n ' ') ++ (varPrint a) ++ " := " ++ (exprPrint False b) ++ ";\n"
@@ -72,6 +84,7 @@ stmtPrint n (IfElse a b c)
 stmtPrint n (While a b)
   = (replicate n ' ') ++ "while " ++ (exprPrint False a) ++ " do\n" ++ (foldl (++) "" (map (stmtPrint (n + 4)) b)) ++ (replicate n ' ') ++ "od\n"
 
+-- Function which transforms a variable into a string.
 varPrint :: Var -> String
 varPrint (Id a)
   = a
@@ -80,6 +93,7 @@ varPrint (Array a b)
 varPrint (Matrix a b c)
   = a ++ "[" ++ (exprPrint False b) ++ ", " ++ (exprPrint False c) ++ "]"
 
+-- Function which transforms an expression list into a string.
 exprListPrint :: [Expr] -> String
 exprListPrint []
   = ""
@@ -88,6 +102,15 @@ exprListPrint [x]
 exprListPrint (x:y:ys)
   = (exprPrint False x) ++ ", " ++ (exprListPrint (y:ys))
 
+-- Function which transforms a single expression into a string.
+-- In order to print well-formed parentheses, a boolean flag is maintained.
+-- False represents being at the top level, or outermost position of an expression, in which case parentheses are not needed.
+-- True represents being at any low level, or inner position of an expression, in which case parentheses might be needed.
+-- Notice about unary operators negataion(!) and minus(-):
+-- Although not mentioned in the specification, we adopt the convention that !true and !false are not parenthesized in any case,
+-- which means well-formed output might look like: !true || !false.
+-- And -x, where x stands for an arbitrary expression, is not parenthesized if it appears as the top level of an expression,
+-- or as the first operand of a binary operation, which means well-formed output might look like: -1, -(1 * 2), -1 + (-2 * 3).
 exprPrint :: Bool -> Expr -> String
 exprPrint _ (BoolConst True)
   = "true"
@@ -116,12 +139,14 @@ exprPrint False (Binary a b c)
 exprPrint True (Binary a b c)
   = "(" ++ (exprPrint True b) ++ (binPrint a) ++ (exprPrint True c) ++ ")"
 
+-- Function which transforms a unary operator into a string
 unaPrint :: UnaOp -> String
 unaPrint Neg
   = "!"
 unaPrint Minus
   = "-"
 
+-- Function which transform a binary operator into a string
 binPrint :: BinOp -> String
 binPrint Add
   = " + "
